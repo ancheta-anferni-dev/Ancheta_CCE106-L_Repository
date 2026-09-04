2ab5417cd964504383dfd8e045e0f896742c286d
Zerni <a.ancheta.142888.tc@umindanao.edu.ph>
2026-09-04 14:42:55 +0800

    Ancheta_CoffeeOrderingApp

---
+++ Ancheta_CoffeeOrderingApp/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+export default function App() {
+  const [score, setScore] = useState(0);
+  
+  const handWIn = () => {
+    setScore(score + 1);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
